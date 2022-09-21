6eee83d6d2d7cd1514e474b446193029b9c16e68
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-21 12:05:18 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var startBtn
+function setup() {
+  createCanvas(400, 400);
+  startBtn = createButton("Click to Start")
+  startBtn.position(10,10)
+  startBtn.mousePressed(makeGrid);
+}//end setup
+function draw() {
+  background(220);
+}//end draw
+function makeGrid(){
+  
+}//end makeGrid
