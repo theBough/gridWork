0fdf07c60728bdf9807340037b14d158726e7db3
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-22 11:10:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,16 @@
 var startBtn
+var b;
 function setup() {
   createCanvas(400, 400);
   startBtn = createButton("Click to Start")
   startBtn.position(10,10)
   startBtn.mousePressed(makeGrid);
+  makeGrid()
 }//end setup
 function draw() {
   background(220);
+  b.display();
 }//end draw
 function makeGrid(){
-  
+  b=new Boxes(50,50,100,100,25,225,31)
 }//end makeGrid
