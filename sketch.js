b1125f8be11fe24dd426d55e6999321869aea257
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-09-28 12:02:38 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ function setup() {
   startBtn = createButton("Click to Start");
   startBtn.position(10, 10);
   startBtn.mousePressed(makeGrid);
-  makeGrid();
+  //makeGrid();
 } //end setup
 function draw() {
   background(220);
@@ -15,23 +15,30 @@ function draw() {
       b[i][j].display();
     } //endj loop
   } //end i loop
-  console.log(b)
+  
 } //end draw
 function makeGrid() {
   /*
   the value that follow i< will be the number
   of boxes in each row
   */
-  for (i = 0; i < 10; i++) {
-    /*
-    the value that follows the i* is how far apart each
-    box is.
-    */
+  
+  for (i = 0; i < 5; i++) {
     b[i] = [];
-    //b.push(new Boxes(i * 16, 50, 15, 15, 25, 225, 31));
-    
-    for (j = 0; j < 10; j++) {
-      b[i][j] = new Boxes(i * 16, j * 16, 15, 15, 25, 225, 31);
+      /*
+      The value after the j<
+      is the amount columns
+      */
+    for (j = 0; j < 5; j++) {
+      /*
+      The value that follows the i* is how far apart each
+      box is.
+      The number that follows the j* determines the space between each 
+      box. As far as the heght.
+      The values "15" are the width and height
+      The last 3 values are the rgb values.
+      */
+      b[i][j] = new Boxes(i * 16, j * 16, 15, 15, 0, 225, 255);
     } //end j loop
   } //end i loop
 } //end makeGrid
