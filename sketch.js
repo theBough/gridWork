f457179f53410a78ff3a2caed2924a5ee05f9cb5
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-10-04 11:16:18 -0500

    Added Two red aRed Squares

--- sketch.js
+++ sketch.js
@@ -1,15 +1,12 @@
-var startBtn;
+
 var b = [];
 function setup() {
-  createCanvas(1000, 1000);
-  startBtn = createButton("Click to Start");
-  startBtn.position(10, 10);
-  startBtn.mousePressed(makeGrid);
-  
-  //makeGrid();
+  createCanvas(500, 500);
+  makeGrid();
+  loadColours();
 } //end setup
 function draw() {
-  background(220);
+  background(255);
 
   for (i = 0; i < b.length; i++) {
     for (j = 0; j < b[i].length; j++) {
@@ -21,18 +18,19 @@ function draw() {
   
 } //end draw
 function makeGrid() {
+ 
   /*
   the value that follow i< will be the number
   of boxes in each row
   */
   
-  for (i = 0; i < 10; i++) {
+  for (i = 0; i < 5; i++) {
     b[i] = [];
       /*
       The value after the j<
       is the amount columns
       */
-    for (j = 0; j < 10; j++) {
+    for (j = 0; j < 5; j++) {
       /*
       The value that follows the i* is how far apart each
       box is.
@@ -41,10 +39,22 @@ function makeGrid() {
       The values "15" are the width and height
       The last 3 values are the rgb values.
       */
-      b[i][j] = new Boxes(i * 31, j * 31, 30,30,256, 183, 3);
+      b[i][j] = new Boxes(i * 80, j * 80, 75,75,0, 0,0);
    
     } //end j loop
   } //end i loop
 } //end makeGrid
+function loadColours(){
+  //the first value is the column
+  //the second value is the row
+  b[3][0].r = 255
+  b[3][0].g = 0
+  b[3][0].b = 0
+  
+  b[1][3].r = 255
+  b[1][3].g = 0
+  b[1][3].b = 0
+}//end loadColours
+
 
 
