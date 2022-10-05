cb71fbd44197afe520d0e4bf093ee0471a85fede
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-10-05 11:40:55 -0500

    Update box.js

--- box.js
+++ box.js
@@ -1,4 +1,4 @@
-function Boxes(x,y,w,h,r,g,b){
+function Boxes(x, y, w, h, r, g, b) {
   this.x = x;
   this.y = y;
   this.w = w;
@@ -6,23 +6,26 @@ function Boxes(x,y,w,h,r,g,b){
   this.r = r;
   this.g = g;
   this.b = b;
-  this.display = function(){
-    push()
-    let c  = color(this.r,this.g,this.b);
-    fill(c)
+  this.display = function () {
+    push();
+    let c = color(this.r, this.g, this.b);
+    fill(c);
     rect(this.x, this.y, this.w, this.h);
-    pop()
-  }
-  this.mouseOver = function(){
-    if(mouseY > this.y &&
-       mouseY < this.y + this.h &&
-       mouseX > this.x &&
-       mouseX< this.x + this.w
-      ){
-      //This mouse should be in a box
-      this.r = 0;
-      this.g = 0;
-      this.b = 0
-    }//end if
-  }//end mouseOver
+    pop();
+  };
+  this.mouseOver = function () {
+    if (gameOn) {
+      if (
+        mouseY > this.y &&
+        mouseY < this.y + this.h &&
+        mouseX > this.x &&
+        mouseX < this.x + this.w
+      ) {
+        //This mouse should be in a box
+        this.r = 0;
+        this.g = 0;
+        this.b = 0;
+      } //end if
+    } //end GameOn
+  }; //end mouseOver
 }
